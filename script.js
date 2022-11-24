b1910c21c590205e466a663d937ec93cff874c14
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-11-24 21:01:42 +0100

    Add objects and assign prototypes

--- script.js
+++ script.js
@@ -26,3 +26,23 @@ function bookconstr(title, author, pagenum, readstatus){
         return this.title + " " + this.author + "," + this.pagenum + " and also:" + readstatus
     }
 }
+
+let head = {
+    glasses: 1
+  };
+  
+  let table = {
+    pen: 3,
+    __proto__:head
+  };
+  
+  let bed = {
+    sheet: 1,
+    pillow: 2,
+    __proto__:table
+  };
+  
+  let pockets = {
+    money: 2000,
+    __proto__: bed
+  };
